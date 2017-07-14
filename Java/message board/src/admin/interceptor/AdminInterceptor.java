d5a01d481532c09606f4fce93ed0ca60d4e0575e
laoxiu001 <316075960@qq.com>
2017-07-14 20:00:01 +0800

    不知道有没有更新过，先push一下

--- Java/message board/src/admin/interceptor/AdminInterceptor.java
+++ Java/message board/src/admin/interceptor/AdminInterceptor.java
@@ -9,6 +9,13 @@ import com.opensymphony.xwork2.ActionInvocation;
 import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 
 public class AdminInterceptor extends AbstractInterceptor {
+	/**
+	 * 验证是否登录的拦截器
+	 * login_sta = "OK"：已登录
+	 * login_sta = "WRONG"：未登录
+	 * */
+	
+	private static final long serialVersionUID = 1L;
 	@Override
 	public String intercept(ActionInvocation invocation) throws Exception {
 		//获取session对象
@@ -16,13 +23,18 @@ public class AdminInterceptor extends AbstractInterceptor {
 		//获取用户登录状态
 		String login_sta = (String) session.getAttribute("login_sta");
 		
-		// 如果没有登陆，或者登陆所有的用户名不是yuewei，都返回重新登陆
+		//登录失败
 		if (login_sta == null || !login_sta.equals("OK")) {
 			System.out.println("用户未登录，访问了禁止的后台");
+			System.out.println("拦截器中login_sta状态为：" + login_sta);
 			session.setAttribute("login_msg", "对不起，请先登录");
 			return Action.LOGIN;
+		}else{
+			//用户已登录，拦截器通过
+			System.out.println("拦截器中login_sta状态为：" + login_sta);
+			System.out.println("用户已登录，拦截器通过");
+			return invocation.invoke();
 		}
-		//跳转返回登录页面
-		return invocation.invoke();
+		// 方法在拦截器被垃圾回收之前调用，用来回收init方法初始化的资源
 	}
 }
